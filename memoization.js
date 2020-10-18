f8535336e2657f0ef05443b88456db6c0bffe831
123JackCole <123jack.cole@gmail.com>
2020-10-17 22:45:47 -0700

    added memoized fibonacci funciton

--- memoization.js
+++ memoization.js
@@ -3,3 +3,22 @@ const fibonacci = (n) => {
     return n === 0 || n === 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+console.log(fibonacci(4));
+
+const fibonacciMemo = (n) => {
+    const memo = {};
+
+    const fib = (n) => {
+        let number;
+        if (n in memo) {
+            number = memo[n];
+        } else {
+            n === 0 || n === 1 ? number = n : number = fib(n - 1) + fib(n - 2);
+        }
+        return number;
+    }
+
+    return fib(n);
+}
+
+console.log(fibonacciMemo(4));
