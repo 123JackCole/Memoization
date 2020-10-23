e9eab9d82feef40d94e0aaf9e45774d6684bedfc
123JackCole <123jack.cole@gmail.com>
2020-10-22 22:12:33 -0700

    commenting out unfinished code

--- memoization.js
+++ memoization.js
@@ -27,22 +27,22 @@ console.log(fibonacciMemoMultiple(4));
 
 // memoization with multiple variables
 
-const fibonacciMemoizedMultiple = (n) => {
-    const memo = new Map();
+// const fibonacciMemoizedMultiple = (n) => {
+//     const memo = new Map();
 
-    const fib = (n) => {
-        let number;
+//     const fib = (n) => {
+//         let number;
 
-        if (memo.has(n)) {
-            memo.set(n, number);
-        } else {
-            n === 0 || n === 1 ? number = n : number = fib(n - 1) + fib(n - 2);
-        }
+//         if (memo.has(n)) {
+//             memo.set(n, number);
+//         } else {
+//             n === 0 || n === 1 ? number = n : number = fib(n - 1) + fib(n - 2);
+//         }
 
-        return number;
-    }
+//         return number;
+//     }
 
-    return fib(n);
-}
+//     return fib(n);
+// }
 
-console.log(fibonacciMemoMultiple(4));
+// console.log(fibonacciMemoMultiple(4));
